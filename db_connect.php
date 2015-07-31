0e6b1715605ead2f164f6525ade9865d109a4ff4
Matthew Reat <matthew.reat@gmail.com>
2015-07-31 15:07:16 -0500

    Committing updated PDO connection files with constants

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,17 @@
 <?php
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'codeup');
+// $dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'codeup');
+
+// define ('DB_HOST', 'mysql:host=127.0.0.1');
+// define ('DB_NAME', 'dbname=employees');
+// define ('DB_USER', 'codeup');
+// define ('DB_PASS', 'codeup');
+
+$dbc = new PDO (DB_HOST . ';' . DB_NAME, DB_USER, DB_PASS);
 
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
 
-echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+// echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
--- pdo_test.php
+++ pdo_test.php
@@ -1,2 +1,9 @@
 <?php
+
+define ('DB_HOST', 'mysql:host=127.0.0.1');
+define ('DB_NAME', 'dbname=employees');
+define ('DB_USER', 'codeup');
+define ('DB_PASS', 'codeup');
 require_once 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
